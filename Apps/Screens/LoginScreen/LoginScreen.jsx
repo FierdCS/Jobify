1123e2a5e1daca9ddd685a84c6d2d445c96ba977
jebediahtoblerone <xeh606@gmail.com>
2024-12-23 20:43:23 -0500

    fixed title

--- Apps/Screens/LoginScreen/LoginScreen.jsx
+++ Apps/Screens/LoginScreen/LoginScreen.jsx
@@ -24,7 +24,7 @@ export default function LoginScreen() {
             <View style={styles.contentContainer}>
                 <View style={styles.textContainer}>
                     <Text style={styles.titleText}>Jobify </Text>
-                    <Text style={styles.subtitleText}>Ultimate place to explore your potential</Text>
+                    <Text style={styles.subtitleText}>The Ultimate place to fulfill your potential</Text>
 
                     {/* Sign-in Buttons */}
                     <View style={styles.buttonContainer}>
