c710e52a6aaee7ddca969d0cac52be5657f62113
Abe <114424880+abmilo@users.noreply.github.com>
2025-01-01 08:44:39 -0500

    added auth logic but alreadt outdated

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { useFonts } from 'expo-font';
-import LoginScreen from './Apps/Screens/LoginScreen/LoginScreen';
-import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import LoginScreen from "./Apps/Screens/LoginScreen/LoginScreen";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 export default function App() {
   const [loaded, error] = useFonts({
-    'outfit': require('./assets/fonts/Outfit-Regular.ttf'),
-    'outfit-medium': require('./assets/fonts/Outfit-Medium.ttf'),
-    'outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),
+    outfit: require("./assets/fonts/Outfit-Regular.ttf"),
+    "outfit-medium": require("./assets/fonts/Outfit-Medium.ttf"),
+    "outfit-bold": require("./assets/fonts/Outfit-Bold.ttf"),
   });
   return (
-    <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <View style={styles.container}>
-        <LoginScreen />
+        <SignedIn>
+          <Text>Your Logged In</Text>
+        </SignedIn>
+        <SignedOut>
+          <LoginScreen />
+        </SignedOut>
       </View>
     </ClerkProvider>
   );
@@ -21,7 +28,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-
+    backgroundColor: "#fff",
   },
 });
